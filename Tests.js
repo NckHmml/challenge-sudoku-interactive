115cd86ece6100b91f2ffa7bbe20493bf2b870b0
NckHmml <NckHmml@users.noreply.github.com>
2016-01-15 12:53:43 +0900

    Optimized test code

--- Tests.js
+++ Tests.js
@@ -6,32 +6,31 @@ var env = require("jsdom").env,
     app = express();
 
 describe("Test", function () {
-    var listener;
+    var listener, $, window;
     before(function (done) {
         app.use(express.static("public"));
 
         listener = app.listen(0, function () {
-            done();
+            env({
+                url: "http://127.0.0.1:" + listener.address().port + "/index.html",
+                features: {
+                    FetchExternalResources: ['script'],
+                    ProcessExternalResources: ['script']
+                },
+                done: function (errors, w) {
+                    window = w;
+                    $ = require("jquery")(window);
+                    done();
+                }
+            });
         });
     });
 
     it("if solves puzzle", function (done) {
-        env({
-            url: "http://127.0.0.1:" + listener.address().port + "/index.html",
-            features: {
-                FetchExternalResources: ['script'],
-                ProcessExternalResources: ['script']
-            },
-            done: function (errors, window) {
-                var $ = require("jquery")(window);
-
-                $("#btnSolve").click();
-                $(window.sudoku.cells).each(function () {
-                    assert.notEqual("", $(this).html());
-                });
-                console.log($("body").html());
-                done();
-            }
+        $("#btnSolve").click();
+        $(window.sudoku.cells).each(function () {
+            assert.notEqual("", $(this).html());
         });
+        done();
     });
 });
